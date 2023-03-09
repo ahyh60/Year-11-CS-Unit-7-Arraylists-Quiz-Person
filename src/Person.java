aeeec3fe25435bda58d004fd41c07444d6b0dbbb
amandaho <ahyh60@student.cis.edu.hk>
2023-03-09 14:44:51 +0800

    person class added

--- src/Person.java
+++ src/Person.java
@@ -4,4 +4,31 @@ public class Person {
     private String lastName;
     private int age;
 
+    public Person(String f, String l, int a){
+        firstName = f;
+        lastName = l;
+        age = a;
+    }
+
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setFirstName(String f){
+        firstName = f;
+    }
+    public void setLastName(String l){
+        lastName = l;
+    }
+    public void setAge(int a){
+        age = a;
+    }
+    public String toString(){
+        return "First name = " + firstName + ", Last name = " + lastName + ", age = " + age;
+    }
 }
